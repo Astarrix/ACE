c27ab5f0d375c275b2016940b7e79ff1f9bef4a9
Astarrix <60230113+Astarrix@users.noreply.github.com>
2025-05-27 02:30:12 +0100

    Delete SDL_Engine directory

--- SDL_Engine/SDL_Engine/Characters/SubCharacters/Character_Mario.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Characters/Character.h"
-
-class Character_Mario : public Character
-{
-public:
-	Character_Mario(SDL_Renderer* renderer, SpriteData spriteData, ACE_Vector2D initPosition = ACE_Vector2D(0.0, 0.0), ACE_LevelMap* map = nullptr);
-	void Update(float deltaTime, SDL_Event* event) override;
-};
